0579f6d58534a11126c1a2fcfa6d6eb8da59a652
develdoe <andreeray@live.com>
2024-10-16 12:36:21 +0200

    Update at 2024-10-16T10:36:21.886Z

---
+++ src/views/WebSocketClient.vue
@@ -0,0 +1,46 @@
+<template>
+	<div>
+		<h1>WebSocket Client</h1>
+		<p>Status: {{ connectionStatus }}</p>
+		<input v-model="message" placeholder="Type a message" />
+		<button @click="sendMessage">Send Message</button>
+		<p>Server Response: {{ serverResponse }}</p>
+	</div>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			socket: null,
+			message: "",
+			serverResponse: "",
+			connectionStatus: "Disconnected",
+		};
+	},
+	methods: {
+		connectWebSocket() {
+			// Sreate a WebSocket connection
+			this.socket = new WebSocket("ws://localhost:4000");
+
+			// Handle WebSocket events
+			this.socket.onopen = () => {
+				this.connectionStatus = "Connected";
+			};
+
+			this.socket.onmessage = (event) => {
+				this.serverResponse = event.data;
+			};
+
+			this.socket.onclose = () => {
+				this.connectionStatus = "Disconnected";
+			};
+
+			this.socket.onerror = (error) => {
+				console.error("WebSocket Error:", error);
+				this.connectionStatus = "Error";
+			};
+		},
+	},
+};
+</script>
