55117956112a61f8543e89c830da9f8ac25098c7
develdoe <andreeray@live.com>
2024-10-26 16:23:38 +0200

    Update at 2024-10-26T14:23:38.341Z

--- src/components/UploadOrders.vue
+++ src/components/UploadOrders.vue
@@ -105,7 +105,7 @@ export default {
 
 				// Set message and update trades in the chart for immediate display
 				this.message = "Orders successfully imported!";
-				this.filteredTrades = newTrades; // Directly use new trades for the chart
+				this.trades = newTrades; // Directly use new trades for the chart
 			} catch (error) {
 				this.message = "Failed to import orders.";
 				console.error(error);
