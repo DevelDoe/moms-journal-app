6d1b993ee92ad556f6524970cc96ef2daef87a15
develdoe <andreeray@live.com>
2024-10-26 17:12:36 +0200

    Update at 2024-10-26T15:12:36.607Z

--- src/components/partials/reports/FullscreenCulmReport.vue
+++ src/components/partials/reports/FullscreenCulmReport.vue
@@ -149,7 +149,7 @@ export default {
 						type: "line",
 						data: this.cumulativeProfitData.profitData.map((value) => parseFloat(value.toFixed(2))),
 						itemStyle: { color: "#e57373" },
-						smooth: false,
+						smooth: true,
 					},
 					{
 						name: "Number of Trades",
