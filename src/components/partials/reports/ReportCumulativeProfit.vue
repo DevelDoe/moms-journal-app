bc2191e10b97fc43548354d7ff8ae68b3fb5c891
develdoe <andreeray@live.com>
2024-10-26 21:24:09 +0200

    Update at 2024-10-26T19:24:09.318Z

--- src/components/partials/reports/ReportCumulativeProfit.vue
+++ src/components/partials/reports/ReportCumulativeProfit.vue
@@ -165,7 +165,7 @@ export default {
 						name: "Number of Trades",
 						position: "right",
 						axisLine: {
-							lineStyle: { color: "#1E3E62" },
+							lineStyle: { color: "#eaeaea" },
 						},
 						axisLabel: {
 							color: "#1E3E62",
