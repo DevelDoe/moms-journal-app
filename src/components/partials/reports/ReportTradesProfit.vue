2738cffea8315e7f1ec11c5feaa714b500347c78
develdoe <andreeray@live.com>
2024-10-26 23:16:20 +0200

    Update at 2024-10-26T21:16:19.989Z

--- src/components/partials/reports/ReportTradesProfit.vue
+++ src/components/partials/reports/ReportTradesProfit.vue
@@ -28,7 +28,7 @@ export default {
 		},
 		granularity: {
 			type: String,
-			default: "hourly", // Options: 'hourly', 'daily', 'weekly', 'monthly', 'yearly'
+			default: "daily", // Options: 'hourly', 'daily', 'weekly', 'monthly', 'yearly'
 		},
 	},
 	components: {
