882256a32297513f9473b5965b9e0adb75d53043
develdoe <andreeray@live.com>
2024-10-26 23:22:01 +0200

    Update at 2024-10-26T21:22:01.408Z

--- src/components/partials/reports/ReportTradesProfit.vue
+++ src/components/partials/reports/ReportTradesProfit.vue
@@ -28,7 +28,7 @@ export default {
 		},
 		granularity: {
 			type: String,
-			default: "hourly", // Options: 'hourly', 'daily', 'weekly', 'monthly', 'yearly'
+			default: "weekly", // Options: 'hourly', 'daily', 'weekly', 'monthly', 'yearly'
 		},
 	},
 	components: {
