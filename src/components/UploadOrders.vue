574862f5ae0dc9ce38008eb978a4c79aa53bd325
develdoe <andreeray@live.com>
2024-10-26 17:19:00 +0200

    Update at 2024-10-26T15:19:00.394Z

--- src/components/UploadOrders.vue
+++ src/components/UploadOrders.vue
@@ -5,7 +5,7 @@
 		<button @click="importOrders" :disabled="!orders.length">Import Orders</button>
 
 		<!-- Display the cumulative profit report for the uploaded trades -->
-		<FullScreenCulmReport v-if="trades && trades.length > 0" :trades="trades" granularity="granularity" />
+		<FullScreenCulmReport v-if="trades && trades.length > 0" :trades="trades" :granularity="granularity" />
 			
 		<!-- Display the list of uploaded orders -->
 		<table v-if="orders.length > 0" class="orders-table">
