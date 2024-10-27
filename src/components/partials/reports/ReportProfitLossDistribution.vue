eba200e7b6e5cfb191e50c79fc643552d458b59d
develdoe <andreeray@live.com>
2024-10-27 19:41:36 +0100

    Update at 2024-10-27T18:41:36.923Z

--- src/components/partials/reports/ReportProfitLossDistribution.vue
+++ src/components/partials/reports/ReportProfitLossDistribution.vue
@@ -148,7 +148,7 @@ export default {
 						},
 						itemStyle: {
 							color: (params) => {
-								const colors = ["#740938", "#AF1740", "#CC2B52", "#DE7C7D"];
+								const colors = ["#740938", "#AF1740", "#CC2B52", "#740938"];
 								return colors[params.dataIndex % colors.length];
 							},
 							shadowBlur: 100,
