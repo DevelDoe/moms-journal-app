1ef3ba8e723544ed78452ed3f13379f8daf4a094
develdoe <andreeray@live.com>
2024-10-27 20:12:58 +0100

    Update at 2024-10-27T19:12:58.771Z

--- src/components/partials/reports/ReportProfitLossAccuracy.vue
+++ src/components/partials/reports/ReportProfitLossAccuracy.vue
@@ -50,48 +50,32 @@ export default {
 						endAngle: 0,
 						min: 0, // Set min lower to center 1:1
 						max: 2, // Adjust max to center 1:1
-						// progress: { show: true, width: 10 },
+						progress: { show: true, width: 10 },
 						axisLine: {
 							lineStyle: {
-								width: 30,
+								width: 10,
 								color: [
-									[0.3, "#67e0e3"],
-									[0.7, "#37a2da"],
-									[1, "#fd666d"],
+									[0.5, "#740938"],
+									[1, "#72BF78"],
 								],
 							},
 						},
-						pointer: {
-							itemStyle: {
-								color: "auto",
-							},
-						},
 						axisTick: {
-							distance: -30,
-							length: 8,
-							lineStyle: {
-								color: "#fff",
-								width: 2,
-							},
-						},
-						splitLine: {
-							distance: -30,
-							length: 30,
-							lineStyle: {
-								color: "#fff",
-								width: 4,
-							},
-						},
-						axisLabel: {
-							color: "inherit",
-							distance: 40,
-							fontSize: 20,
-						},
-						detail: {
-							valueAnimation: true,
-							formatter: "{value} km/h",
-							color: "inherit",
-						},
+        distance: -30,
+        length: 8,
+        lineStyle: {
+          color: '#fff',
+          width: 2
+        }
+      },
+	  splitLine: {
+        distance: -30,
+        length: 30,
+        lineStyle: {
+          color: '#fff',
+          width: 4
+        }
+      },
 						pointer: { width: 1 },
 						title: { show: false },
 						detail: {
