25ee50f926c65629425ec88e6fe6482e387e947e
develdoe <andreeray@live.com>
2024-10-27 13:59:25 +0100

    Update at 2024-10-27T12:59:25.747Z

--- src/components/partials/reports/ReportProfitsByTime.vue
+++ src/components/partials/reports/ReportProfitsByTime.vue
@@ -94,6 +94,17 @@ export default {
 			return {
 				title: {
 					text: `Trades by ${this.selectedGranularity === "hourly" ? "Hour" : "Minute"}`,
+					left: "left", // Position the title in the center
+					textStyle: {
+						color: "#ffffff", // Title text color
+						fontSize: 18, // Font size
+						fontWeight: "bold", // Font weight: 'normal', 'bold', 'bolder', 'lighter'
+					},
+					subtext: "", // Add a subtitle if needed
+					subtextStyle: {
+						color: "#aaa", // Subtitle color
+						fontSize: 14,
+					},
 				},
 				tooltip: {
 					trigger: "axis",
