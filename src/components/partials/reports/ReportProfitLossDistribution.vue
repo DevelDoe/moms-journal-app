1bd314799c7f3f4702026750b7204f28d721e145
develdoe <andreeray@live.com>
2024-10-27 11:11:24 +0100

    Update at 2024-10-27T10:11:24.025Z

--- src/components/partials/reports/ReportProfitLossDistribution.vue
+++ src/components/partials/reports/ReportProfitLossDistribution.vue
@@ -112,6 +112,13 @@ export default {
 						fontSize: 10, // Size of the font for the labels
 					},
 					name: "Profit/Loss",
+					splitLine: {
+						lineStyle: {
+							color: "#1E3E62", // Change this to your preferred color for the horizontal lines
+							width: 1, // Thickness of the lines
+							type: "solid", // Options: 'solid', 'dashed', 'dotted'
+						},
+					},
 				},
 				yAxis: {
 					type: "category",
