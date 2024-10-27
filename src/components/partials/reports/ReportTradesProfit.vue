6ad6cfcc3930f56bc94394b2a0506bd6835305c0
develdoe <andreeray@live.com>
2024-10-27 19:44:51 +0100

    Update at 2024-10-27T18:44:51.871Z

--- src/components/partials/reports/ReportTradesProfit.vue
+++ src/components/partials/reports/ReportTradesProfit.vue
@@ -182,7 +182,7 @@ export default {
 						stack: "Total",
 						data: tradeCountData.map((value) => ({
 							value: parseFloat(value).toFixed(2),
-							itemStyle: { color: value >= 0 ? "#FF9800" : "#FEFF9F" },
+							itemStyle: { color: value >= 0 ? "#FEFF9F" : "#FEFF9F" },
 						})),
 					},
 					{
