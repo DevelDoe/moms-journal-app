43923354540ebdfc489751727e2679da0e26913e
develdoe <andreeray@live.com>
2024-10-27 20:11:32 +0100

    Update at 2024-10-27T19:11:32.272Z

--- src/components/partials/reports/ReportProfitLossAccuracy.vue
+++ src/components/partials/reports/ReportProfitLossAccuracy.vue
@@ -62,7 +62,11 @@ export default {
 						},
 						pointer: { width: 1 },
 						title: { show: false },
-						
+						detail: {
+							formatter: () => this.profitToLossRatio,
+							fontSize: 20,
+							color: "#eaeaea",
+						},
 						data: [{ value: parseFloat(this.profitToLossRatio.split(":")[0]) }],
 					},
 				],
