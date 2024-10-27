c07ca821951e7083e32821ce5036692dc94ef709
develdoe <andreeray@live.com>
2024-10-27 18:33:20 +0100

    Update at 2024-10-27T17:33:20.638Z

--- src/components/partials/reports/ReportProfitLossDistribution.vue
+++ src/components/partials/reports/ReportProfitLossDistribution.vue
@@ -101,7 +101,7 @@ export default {
 					{
 						name: "Profit/Loss",
 						type: "pie",
-						radius: ["0%", "50%"],
+						radius: ["0%", "90%"],
 						center: ["50%", "50%"],
 						roseType: "radius",
 						data: this.profitByPriceRange.data.map((item) => {
