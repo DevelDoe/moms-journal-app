98118df44f183d6ae9db0d02c1e159934c851db4
develdoe <andreeray@live.com>
2024-10-27 18:42:35 +0100

    Update at 2024-10-27T17:42:35.094Z

--- src/components/partials/reports/ReportProfitLossDistribution.vue
+++ src/components/partials/reports/ReportProfitLossDistribution.vue
@@ -136,7 +136,7 @@ export default {
 									y2: 1,
 									colorStops: [
 										{ offset: 0, color: colors[params.dataIndex % colors.length] },
-										{ offset: 0.5, color: "rgba(0,0,0,0.1)" },
+										{ offset: 0.5, color: "FF6500" },
 									],
 								};
 							},
