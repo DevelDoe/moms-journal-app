09a60d3354d2e8ca63d569ed77d0956921b3e221
develdoe <andreeray@live.com>
2024-10-27 18:49:56 +0100

    Update at 2024-10-27T17:49:56.400Z

--- src/components/partials/reports/ReportTradesProfit.vue
+++ src/components/partials/reports/ReportTradesProfit.vue
@@ -196,7 +196,7 @@ export default {
 						},
 						data: profitData.map((value) => ({
 							value: parseFloat(value).toFixed(2),
-							itemStyle: { color: value >= 0 ? "#6A9C89" : "#820300" },
+							itemStyle: { color: value >= 0 ? "#5F8670" : "#820300" },
 						})),
 					},
 				],
