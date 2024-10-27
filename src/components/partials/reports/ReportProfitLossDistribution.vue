33a755fe84bdd5068e22c575f96138aba4cfd5a6
develdoe <andreeray@live.com>
2024-10-27 18:40:45 +0100

    Update at 2024-10-27T17:40:45.684Z

--- src/components/partials/reports/ReportProfitLossDistribution.vue
+++ src/components/partials/reports/ReportProfitLossDistribution.vue
@@ -127,7 +127,7 @@ export default {
 						itemStyle: {
 							// Dynamic gradient for each segment based on value
 							color: (params) => {
-								const colors = ["#393E46", "#FFD271", "#00ADB5", "#CC2B52", "#A04747", "#D8A25E"];
+								const colors = ["#393E46", "#FFD271", "#00ADB5", "#CC2B52", "#F05A7E", "#D8A25E"];
 								return {
 									type: "radiant",
 									x: 0,
