95cdf6ec746b26f9c162094f3244e17837f389cb
develdoe <andreeray@live.com>
2024-10-27 14:29:02 +0100

    Update at 2024-10-27T13:29:02.114Z

--- src/components/partials/reports/ReportProfitLossDistribution.vue
+++ src/components/partials/reports/ReportProfitLossDistribution.vue
@@ -102,7 +102,7 @@ export default {
 							lineStyle: {
 								color: "rgba(255, 255, 255, 0.3)",
 							},
-							smooth: 0.1,
+							smooth: 0.01,
 							length: 100,
 							length2: 200,
 						},
