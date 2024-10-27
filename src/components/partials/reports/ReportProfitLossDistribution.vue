b307f355580e1537e9629a1f059974dab7f1eb7d
develdoe <andreeray@live.com>
2024-10-27 19:41:09 +0100

    Update at 2024-10-27T18:41:09.885Z

--- src/components/partials/reports/ReportProfitLossDistribution.vue
+++ src/components/partials/reports/ReportProfitLossDistribution.vue
@@ -118,7 +118,7 @@ export default {
 						},
 						itemStyle: {
 							color: (params) => {
-								const colors = ["#72BF78", "#A0D683", "#D3EE98", "#1A5319"];
+								const colors = ["#72BF78", "#A0D683", "#508D4E", "#1A5319"];
 								return colors[params.dataIndex % colors.length];
 							},
 							shadowBlur: 100,
